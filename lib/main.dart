c01bde9f97b315e11f349c9a392b7b4df4b86f81
Andrew Steele <andrewsteele@Andrew-Steeles-MacBook-Pro.local>
2019-04-24 23:07:11 -0700

    initial stuff

---
+++ lib/main.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'custom_card.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  TextEditingController taskTitleInputController;
+  TextEditingController taskDescripInputController;
+
+  @override
+  initState() {
+    taskTitleInputController = new TextEditingController();
+    taskDescripInputController = new TextEditingController();
+    super.initState();
+  }
+
+  _showDialog() async {
+    await showDialog<String>(
+      context: context,
+      child: AlertDialog(
+        contentPadding: const EdgeInsets.all(16.0),
+        content: Column(
+            children: <Widget>[
+            Text("Please fill all fields to create a new task"),
+        Expanded(
+          child: TextField(
+            autofocus: true,
+            decoration: InputDecoration(labelText: 'Task Title*'),
+            controller: taskTitleInputController,
+          ),
+        ),
+        Expanded(
+          child: TextField(
+            decoration: InputDecoration(labelText: 'TaskDescription*'),
+              controller: taskDescripInputController,
+            ),
+          )
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                taskTitleInputController.clear();
+                taskDescripInputController.clear();
+                Navigator.pop(context);
+              }),
+          FlatButton(
+              child: Text('Add'),
+              onPressed: () {
+                print(taskTitleInputController.text);
+                print(taskDescripInputController.text);
+                if (taskDescripInputController.text.isNotEmpty &&
+                    taskTitleInputController.text.isNotEmpty) {
+                  Firestore.instance.collection('tasks').add({
+                    "title": taskTitleInputController.text,
+                    "description": taskDescripInputController.text
+                  })
+                      .then((result) => {
+                  Navigator.pop(context),
+                  taskTitleInputController.clear(),
+                  taskDescripInputController.clear(),
+                  })
+                      .catchError((err) => print(err));
+                }
+              })
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+            padding: const EdgeInsets.all(10.0),
+            child: StreamBuilder<QuerySnapshot>(
+              stream: Firestore.instance.collection('tasks')
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError)
+                  return new Text('Error: ${snapshot.error}');
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    return new Text('Loading...');
+                  default:
+                    return new ListView(
+                      children: snapshot.data.documents
+                          .map((DocumentSnapshot document) {
+                        return new CustomCard(
+                          title: document['title'],
+                          description: document['description'],
+                        );
+                      }).toList(),
+                    );
+                }
+              },
+            )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showDialog,
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+}
